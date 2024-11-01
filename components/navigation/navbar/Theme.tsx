26e002251ac10d0fc9fd51cc8914d3132c7f08e0
SabaMarghania1 <marghanias@gmail.com>
2024-11-01 20:03:54 +0400

    Implemented navbar and theme changer

---
+++ components/navigation/navbar/Theme.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Theme = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Image
+            src="/icons/sun.svg"
+            width={16}
+            height={16}
+            alt="Moon"
+            className=" rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Image
+            src="/icons/moon.svg"
+            width={16}
+            height={16}
+            alt="Moon"
+            className="absolute  rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Theme;
