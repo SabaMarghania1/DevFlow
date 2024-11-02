b849803aa7265dd5ce6cf768038707b2b6825c63
SabaMarghania1 <marghanias@gmail.com>
2024-11-02 09:17:44 +0400

    implemented Google auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,8 @@ import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
-
   console.log(session);
+
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
