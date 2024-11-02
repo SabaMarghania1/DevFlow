724da2afab3ab743b6abf22da3cf6e94bb3fbe27
SabaMarghania1 <marghanias@gmail.com>
2024-11-02 08:09:18 +0400

    implement authentication by github and signout functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
-const Home = () => (
-  <>
-    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-  </>
-);
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit" className="mt-[100px] px-10">
+          Log out
+        </Button>
+      </form>
+    </>
+  );
+};
 export default Home;
